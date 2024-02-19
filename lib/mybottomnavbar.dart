a7ce09a020d457ecd2c14702d58c8ec897c15122
Praveen Tiwari <praveen.tiwari@appinevtiv.com>
2024-02-19 15:56:31 +0530

    comment test directory

--- lib/mybottomnavbar.dart
+++ lib/mybottomnavbar.dart
@@ -1,45 +1,12 @@
  library mybottomnavbar;
 import 'package:flutter/material.dart';
 
-// class DynamicBottomNavBar extends StatefulWidget {
-//   final List<NavBarItem> items;
-//   final int currentIndex;
-//   final ValueChanged<int> onTap;
-
-//   const DynamicBottomNavBar({
-//     Key? key,
-//     required this.items,
-//     required this.currentIndex,
-//     required this.onTap,
-//   }) : super(key: key);
-
-//   @override
-//   DynamicBottomNavBarState createState() => DynamicBottomNavBarState();
-// }
-
-// class DynamicBottomNavBarState extends State<DynamicBottomNavBar> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return BottomNavigationBar(
-//       items: widget.items
-//           .map((item) => BottomNavigationBarItem(
-//                 icon: Icon(item.icon),
-//                 label: item.label,
-//               ))
-//           .toList(),
-//       currentIndex: widget.currentIndex,
-//       onTap: widget.onTap,
-//     );
-//   }
-// }
-
-
 class DynamicBottomNavBar extends StatefulWidget {
   final List<NavBarItem> items;
   final int currentIndex;
   final ValueChanged<int> onTap;
-  final Color iconColor;
-  final Color labelColor;
+  final Color? iconColor;
+  final Color? labelColor;
 
   const DynamicBottomNavBar({
     Key? key,
@@ -83,3 +50,52 @@ class NavBarItem {
     required this.label,
   });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class DynamicBottomNavBar extends StatefulWidget {
+//   final List<NavBarItem> items;
+//   final int currentIndex;
+//   final ValueChanged<int> onTap;
+
+//   const DynamicBottomNavBar({
+//     Key? key,
+//     required this.items,
+//     required this.currentIndex,
+//     required this.onTap,
+//   }) : super(key: key);
+
+//   @override
+//   DynamicBottomNavBarState createState() => DynamicBottomNavBarState();
+// }
+
+// class DynamicBottomNavBarState extends State<DynamicBottomNavBar> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return BottomNavigationBar(
+//       items: widget.items
+//           .map((item) => BottomNavigationBarItem(
+//                 icon: Icon(item.icon),
+//                 label: item.label,
+//               ))
+//           .toList(),
+//       currentIndex: widget.currentIndex,
+//       onTap: widget.onTap,
+//     );
+//   }
+// }
+
--- test/mybottomnavbar_test.dart
+++ test/mybottomnavbar_test.dart
@@ -1,55 +1,41 @@
+// import 'package:flutter/material.dart';
 // import 'package:flutter_test/flutter_test.dart';
-
 // import 'package:mybottomnavbar/mybottomnavbar.dart';
 
 // void main() {
-//   test('adds one to input values', () {
-//     final calculator = Calculator();
-//     expect(calculator.addOne(2), 3);
-//     expect(calculator.addOne(-7), -6);
-//     expect(calculator.addOne(0), 1);
+//   testWidgets('DynamicBottomNavBar Widget Test', (WidgetTester tester) async {
+//     final List<NavBarItem> items = [
+//       NavBarItem(icon: Icons.home, label: 'Home'),
+//       NavBarItem(icon: Icons.search, label: 'Search'),
+//       NavBarItem(icon: Icons.favorite, label: 'Favorite'),
+//     ];
+
+//     int selectedIndex = 0;
+//     int tappedIndex = -1;
+
+//     await tester.pumpWidget(
+//       MaterialApp(
+//         home: DynamicBottomNavBar(
+//           items: items,
+//           currentIndex: selectedIndex,
+//           onTap: (index) {
+//             tappedIndex = index;
+//           },
+//         ),
+//       ),
+//     );
+
+//     expect(find.byIcon(Icons.home), findsOneWidget);
+//     expect(find.byIcon(Icons.search), findsOneWidget);
+//     expect(find.byIcon(Icons.favorite), findsOneWidget);
+
+//     expect(selectedIndex, 0);
+//     expect(tappedIndex, -1);
+
+//     await tester.tap(find.byIcon(Icons.search));
+//     await tester.pump();
+
+//     expect(selectedIndex, 0); 
+//     expect(tappedIndex, 1); 
 //   });
 // }
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mybottomnavbar/mybottomnavbar.dart';
-
-
-void main() {
-  testWidgets('DynamicBottomNavBar Widget Test', (WidgetTester tester) async {
-    final List<NavBarItem> items = [
-      NavBarItem(icon: Icons.home, label: 'Home'),
-      NavBarItem(icon: Icons.search, label: 'Search'),
-      NavBarItem(icon: Icons.favorite, label: 'Favorite'),
-    ];
-
-    int selectedIndex = 0;
-    int tappedIndex = -1;
-
-    await tester.pumpWidget(
-      MaterialApp(
-        home: DynamicBottomNavBar(
-          items: items,
-          currentIndex: selectedIndex,
-          onTap: (index) {
-            tappedIndex = index;
-          },
-        ),
-      ),
-    );
-
-    expect(find.byIcon(Icons.home), findsOneWidget);
-    expect(find.byIcon(Icons.search), findsOneWidget);
-    expect(find.byIcon(Icons.favorite), findsOneWidget);
-
-    expect(selectedIndex, 0);
-    expect(tappedIndex, -1);
-
-    await tester.tap(find.byIcon(Icons.search));
-    await tester.pump();
-
-    expect(selectedIndex, 0); // selectedIndex should remain unchanged
-    expect(tappedIndex, 1); // tappedIndex should be updated to 1
-  });
-}

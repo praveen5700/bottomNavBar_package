ade3ee28f9d24c29c800e428d6343480390eab6f
Praveen Tiwari <praveen.tiwari@appinevtiv.com>
2024-02-21 11:21:59 +0530

    added check on user input

--- lib/mybottomnavbar.dart
+++ lib/mybottomnavbar.dart
@@ -1,26 +1,54 @@
- library mybottomnavbar;
+library mybottomnavbar;
+
 import 'package:flutter/material.dart';
 
 class DynamicBottomNavBar extends StatefulWidget {
   final List<NavBarItem> items;
   final int currentIndex;
   final ValueChanged<int> onTap;
-  final Color? iconColor;
-  final Color? labelColor;
-  final Color? backgroundColor;
-  final double? elevation;
+  final Color selectedItemColor;
+  final Color unselectedItemColor;
+  final Color selectedLabelColor;
+  final Color unselectedLabelColor;
+  final Color backgroundColor;
+  final double elevation;
+  final double iconSize;
+  final double selectedFontSize;
+  final double unselectedFontSize;
+  final TextStyle? selectedLabelStyle;
+  final TextStyle? unselectedLabelStyle;
+  final bool? showSelectedLabels;
+  final bool? showUnselectedLabels;
 
   const DynamicBottomNavBar({
     Key? key,
     required this.items,
     required this.currentIndex,
     required this.onTap,
-    this.iconColor = Colors.blue,
-    this.labelColor = Colors.blue, 
-    this.backgroundColor,
-    this.elevation, 
-  }) : assert(items.length >= 2 && items.length <= 6,
-            'The number of items must be between 2 and 6'),
+    this.selectedItemColor = Colors.blue,
+    this.unselectedItemColor = Colors.grey,
+    this.selectedLabelColor = Colors.blue,
+    this.unselectedLabelColor = Colors.grey,
+    this.backgroundColor = Colors.white,
+    this.elevation = 0.0,
+    this.iconSize = 24.0,
+    this.selectedFontSize = 14.0,
+    this.unselectedFontSize = 12.0,
+    this.selectedLabelStyle,
+    this.unselectedLabelStyle,
+    this.showSelectedLabels = true,
+    this.showUnselectedLabels = true,
+  }):assert(
+            items.length >= 2 &&
+                items.length <= 6 &&
+                iconSize > 0 &&
+                iconSize <= 40 &&
+                selectedFontSize > 0 &&
+                selectedFontSize <= 40 &&
+                unselectedFontSize > 0 &&
+                unselectedFontSize <= 40 &&
+                elevation >= 0,
+          'The number of items must be between 2 and 6 and the icon size should be between 0 and 50 pixels'),
         super(key: key);
 
   @override
@@ -38,13 +66,16 @@ class DynamicBottomNavBarState extends State<DynamicBottomNavBar> {
               ))
           .toList(),
       currentIndex: widget.currentIndex,
-      selectedItemColor: widget.iconColor,
-      unselectedItemColor: Colors.grey,
+      selectedItemColor: widget.selectedItemColor,
+      unselectedItemColor: widget.unselectedItemColor,
       onTap: widget.onTap,
-      selectedLabelStyle: TextStyle(color: widget.labelColor),
-      unselectedLabelStyle:const TextStyle(color: Colors.grey),
+      selectedLabelStyle: TextStyle(color: widget.selectedLabelColor),
+      unselectedLabelStyle: TextStyle(color: widget.unselectedLabelColor),
       backgroundColor: widget.backgroundColor,
       elevation: widget.elevation,
+      selectedFontSize: widget.selectedFontSize,
+      unselectedFontSize: widget.unselectedFontSize,
+      
     );
   }
 }
